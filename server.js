457d034ebd503db66c403bfdd0b689ee0a5b98dd
Ujwal Gowda V <ujwalgowda2002v@gmail.com>
2026-04-11 21:16:14 +0530

    Server changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ dotenv.config();
 
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const propertyRoutes = require("./routes/propertyRoutes");
@@ -18,18 +19,17 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 8084;
 
-app.use(cors({
-    origin: ["http://localhost:5173", "https://namaste-be.onrender.com/"],
-    credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const server = http.createServer(app);
 
 // app.use(passport.initialize());
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
 });
-console.log("Entered");
 app.get("/", (req, res) => {
     res.send("API working");
 });
@@ -39,4 +39,4 @@ app.use("/admin", adminRoutes);
 app.use("/utilities", utilityRoutes);
 app.use("/requests", requestRoutes);
 
-app.listen(port, () => console.log("Server running at port:\t" + port));
+server.listen(port, () => console.log("Server running at port:\t" + port));
